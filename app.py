2f9cf633c5abdca0c7bf3c4b3c2303833cb7d9cf
Jimel Joseph <3507684+jimelj@users.noreply.github.com>
2025-04-07 21:10:38 -0400

    working

--- app.py
+++ app.py
@@ -1197,4 +1197,4 @@ def handle_error(e):
     return render_template('error.html', error=str(e)), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
